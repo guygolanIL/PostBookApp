5c4f6e247397f502a6b1bb5a0a262f4cf09aa279
guy.golan <guygolan91@gmail.com>
2019-11-23 20:07:29 +0200

    Add project files.

---
+++ PostBookApp/MainPage.Designer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace PostBookApp
+{
+    partial class MainPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.m_LoginButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // m_LoginButton
+            // 
+            this.m_LoginButton.Location = new System.Drawing.Point(372, 27);
+            this.m_LoginButton.Name = "m_LoginButton";
+            this.m_LoginButton.Size = new System.Drawing.Size(75, 23);
+            this.m_LoginButton.TabIndex = 0;
+            this.m_LoginButton.Text = "Login";
+            this.m_LoginButton.UseVisualStyleBackColor = true;
+            this.m_LoginButton.Click += new System.EventHandler(this.loginClicked);
+            // 
+            // MainPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.m_LoginButton);
+            this.Name = "MainPage";
+            this.Text = "PostBook App";
+            this.ResumeLayout(false);
+
+        }
+
+        private void loginClicked(object sender, EventArgs e)
+        {
+            this.loginAndInit();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button m_LoginButton;
+    }
+}
+
---
+++ PostBookApp/MainPage.cs
@@ -0,0 +1,61 @@
+﻿using FacebookWrapper;
+using FacebookWrapper.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PostBookApp
+{
+    public partial class MainPage : Form
+    {
+
+        User m_LoggedInUser;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void loginAndInit()
+        {
+
+            LoginResult result = FacebookService.Login("817562982032756", 
+
+                "public_profile",
+                "email",
+                "publish_to_groups",
+                "user_birthday",
+                "user_age_range",
+                "user_gender",
+                "user_link",
+                "user_tagged_places",
+                "user_videos",
+                "publish_to_groups",
+                "groups_access_member_info",
+                "user_friends",
+                "user_events",
+                "user_likes",
+                "user_location",
+                "user_photos",
+                "user_posts",
+                "user_hometown"
+                );
+            
+            if (!string.IsNullOrEmpty(result.AccessToken))
+            {
+                m_LoggedInUser = result.LoggedInUser;
+                MessageBox.Show("You are logged in!");
+            }
+            else
+            {
+                MessageBox.Show(result.ErrorMessage);
+            }
+        }
+
+
+    }
+}
